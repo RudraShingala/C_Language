ab8f993a5ac10761e52da756be13812e5198299a
RudraShingala <131174063+RudraShingala@users.noreply.github.com>
2023-04-27 13:35:03 +0530

    Add files via upload

---
+++ A-PATTER.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a;
+	clrscr();
+
+	printf("  * * * * ");
+	printf("\n*         *");
+	printf("\n*         * ");
+	printf("\n* * * * * * ");
+	printf("\n*         *");
+	printf("\n*         *");
+	printf("\n*         *");
+
+	getch();
+}
---
+++ EQ11.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	float x,y,z,ans;
+	clrscr();
+
+	printf("Enter the value of X : ");
+	scanf("%f",&x);
+
+	printf("Enter the value of Y : ");
+	scanf("%f",&y);
+
+	printf("Enter the value of Z : ");
+	scanf("%f",&z);
+
+	ans = (x + y + z)*(x + y + z);
+	printf("(x+y+z)^2 : %f",ans);
+
+	getch();
+}

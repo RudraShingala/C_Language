08fd54fba007b356c81cbdc8e1619c5d3e6b80a7
RudraShingala <rxdrashingala@gmail.com>
2023-04-29 18:13:52 +0530

    swap variables

---
+++ CHAPTER4/SWAP2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a;
+	float b;
+	clrscr();
+
+	printf("Enter the value of b : ");
+	scanf("%d",&b);
+	b=a;
+	printf("\nThe value of b is : %f",a);
+
+	getch();
+}
